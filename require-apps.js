244de2b459c5c3938856dcded744ef643586e2c7
danPixl <danny@vinci.cloud>
2017-05-19 17:05:16 -0600

    make app folder if it doesn't already exist

--- require-apps.js
+++ require-apps.js
@@ -1,5 +1,6 @@
 var requireDirectory = require('require-directory'),
-    os = require('os')
+    os = require('os'),
+    fs = require('fs')
 
 if (process.platform == 'linux') {
     var appInstallFolder = os.homedir() + '/.chromaticApps'
@@ -11,6 +12,10 @@ if (process.platform == 'darwin') {
 
 if (process.platform == 'win32') {
     var appInstallFolder = os.homedir() + '/Chromatic Apps'
+}
+
+if (!fs.existsSync(appInstallFolder)){
+    fs.mkdirSync(appInstallFolder);
 }
 
 module.exports = requireDirectory(module, appInstallFolder)
