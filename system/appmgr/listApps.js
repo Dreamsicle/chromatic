ad95ad692b66a25fe458c4b8a3993695abd24361
danPixl <danny@vinci.cloud>
2017-05-19 22:58:51 -0600

    basic listApps.js (ignored for now)

---
+++ system/appmgr/listApps.js
@@ -0,0 +1,10 @@
+/*var fs = require('fs'),
+    path = require('path')
+    checkAppInstallFolder = require('./system/startup/checkAppInstallFolder.js')
+
+function getDirectories(srcpath) {
+  return fs.readdirSync(srcpath)
+    .filter(file => fs.lstatSync(path.join(srcpath, file)).isDirectory())
+}
+
+console.log(getDirectories(checkAppInstallFolder()))*/
