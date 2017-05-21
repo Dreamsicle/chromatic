3e79c8d5c170874ae90a0e38fddb0183d7526f96
danPixl <danny@vinci.cloud>
2017-05-20 18:20:33 -0600

    make listApps work

--- system/appmgr/listApps.js
+++ system/appmgr/listApps.js
@@ -1,10 +1,10 @@
-/*var fs = require('fs'),
+var fs = require('fs'),
     path = require('path')
-    checkAppInstallFolder = require('./system/startup/checkAppInstallFolder.js')
+    checkAppInstallFolder = require('../utils/startup/checkAppInstallFolder.js')
 
 function getDirectories(srcpath) {
   return fs.readdirSync(srcpath)
     .filter(file => fs.lstatSync(path.join(srcpath, file)).isDirectory())
 }
 
-console.log(getDirectories(checkAppInstallFolder()))*/
+console.log(getDirectories(checkAppInstallFolder()))
